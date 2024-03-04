b31f7dc259adacf7d1fc83d88b387e65ac43cb57
sam1037 <samngai2522@gmail.com>
2024-03-04 16:30:23 +0800

    making the ham. menu for mobile

---
+++ app.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    alert("DOM fully loaded and parsed");
+    let hamBtn = document.getElementById("hamburger-button");
+    hamBtn.value = 'Off';
+    hamBtn.onclick = () => {
+        toggleMenu(hamBtn);
+    }
+});
+
+
+//toggle the menu on and off
+function toggleMenu(hamBtn){
+    alert("toggling menu!");
+    alert(`btn value b4 changing: ${hamBtn.value}`);
+    
+    if (hamBtn.value == "ON"){
+        hamBtn.value = "Off";
+        //modify the layout to display the mobile menu
+    }
+    else{
+        hamBtn.value = "On";
+        //modify the layout to close the mobile menu
+    }
+}
+
+
