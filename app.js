d19c0950f12b5ed3fc5ce7ba72e0a9a1d24b50fe
sam1037 <samngai2522@gmail.com>
2024-03-06 18:58:50 +0800

    fixed the last commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const hamburger = document.querySelector(".hamburger");
     const nav_menu = document.querySelector(".nav-menu");
+    const body = document.querySelector("body");
 
     hamburger.addEventListener("click", ()=>{
         hamburger.classList.toggle("active");
         nav_menu.classList.toggle("active");
+        body.classList.toggle("active");
     })
 
     //remove active class once a link is clicked
@@ -13,12 +15,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
         n => n.addEventListener("click", ()=>{
             hamburger.classList.remove("active");
             nav_menu.classList.remove("active");
+            body.classList.remove("active");
             setTimeout(()=>{}, 1000);
         })
     )
     document.querySelector(".nav-logo").addEventListener("click", ()=>{
         hamburger.classList.remove("active");
         nav_menu.classList.remove("active");
+        body.classList.remove("active");
         setTimeout(()=>{}, 1000);
     })
 });
