a3cc2d6be2534b5548d61fcef21b199c2e15f952
sam1037 <samngai2522@gmail.com>
2024-10-16 23:09:25 +0800

    making one page scrollable

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", (event) => {
+    const heightLimitVh = 80; // Set the height limit in viewport height (vh)
+    const heightLimitPx = window.innerHeight * (heightLimitVh / 100); // Convert vh to pixels
+
     const hamburger = document.querySelector(".hamburger");
     const nav_menu = document.querySelector(".nav-menu");
     const body = document.querySelector("body");
@@ -25,4 +28,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
         body.classList.remove("active");
         setTimeout(()=>{}, 1000);
     })
+
+    //add show more btn to sections over a certain height limit
+    document.querySelectorAll("section").forEach(section => {
+        //check height
+        if (section.scrollHeight < heightLimitPx) {
+            return;
+        }
+        //create the show more btn element and append it
+        const showMoreBtn = document.createElement('div');
+        showMoreBtn.className = 'show-more';
+        showMoreBtn.textContent = 'Show More';
+
+        section.insertAdjacentElement('afterend', showMoreBtn);
+
+        //add hide extra class to the section
+        section.classList.add("hide-extra");
+
+        //event listener for the show more btn
+        showMoreBtn.addEventListener('click', function() {
+            section.style.maxHeight = section.scrollHeight + 'px';
+            this.remove();
+        });
+    })
+
 });
